37aaebaade0fb2b6c991e4e356c59bedbff11a0b
lucas <lucashcardoso26@gmail.com>
2023-02-08 19:14:45 -0300

    add header of New and add noteItem component

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,10 +1,25 @@
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+
+import { Container, Form } from './styles';
 
 export function New() {
 	return (
 		<Container>
 			<Header />
+
+			<main>
+				<Form>
+					<header>
+						<h1>Create note</h1>
+						<a href="/">Back</a>
+					</header>
+						
+					<Input placeholder="Title"/>
+					<Textarea placeholder="Comments" />
+				</Form>
+			</main>
 		</Container>
 	)
 }
