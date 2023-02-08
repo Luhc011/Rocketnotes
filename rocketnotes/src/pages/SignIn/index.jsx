c869bf1d84a80bc53b07338be0ea4be5c185ecfe
lucas <lucashcardoso26@gmail.com>
2023-02-08 12:38:59 -0300

    signIn form add

---
+++ rocketnotes/src/pages/SignIn/index.jsx
@@ -0,0 +1,37 @@
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form } from "./styles";
+
+export function SignIn() {
+	return (
+		<Container>
+			<Form>
+				<h1>Rocket Notes</h1>
+				<p>Application to save and manage your links</p>
+				
+				<h2>Login</h2>
+
+				<Input 
+					placeholder="E-mail"
+					type="email"
+					icon={FiMail}
+				/>
+
+				<Input 
+					placeholder="Password"
+					type="password"
+					icon={FiLock}
+				/>
+
+				<Button title="Enter"/>
+
+				<a href="#">
+					Create account
+				</a>				
+			</Form>			
+		</Container>
+	)
+}
